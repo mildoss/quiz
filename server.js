a5a81ee1b21d5ac6552a507f990b5cd716f06d35
mildoss <vlad.maslo.05@gmail.com>
2025-02-26 23:28:07 +0200

    test1

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.get("/api/flags", (req, res) => {
   res.json(selectedFlags);
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
